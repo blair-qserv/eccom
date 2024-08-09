fb1e15dd52ea5df9033360b3c362256a7b74fd4f
blair-qserv <blair.jurgen_chacon@questronix.com.ph>
2024-08-09 19:46:46 +0800

    first

---
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,22 @@
+import './NewsLetter.css'
+
+const NewsLetter = () => {
+  return (
+    <div className='newsletter'>
+        <h1>
+            Get Exclusive Offers On Your Email
+        </h1>
+        <p>
+            Subscribe to our newsletter and stay update
+        </p>
+        <div>
+            <input type="email" placeholder='Your email' />
+            <button>
+                Subscribe
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default NewsLetter
